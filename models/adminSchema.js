14ecd98a7a3b179eba3b96730d1b49124f24c511
farahani <farahani@jobjoo.io>
2018-11-29 13:27:57 +0330

    just test

---
+++ models/adminSchema.js
@@ -0,0 +1,25 @@
+// mongoose
+const mongoose = require('mongoose');
+
+// destructure Schema from mongoose
+const Schema = mongoose.Schema;
+
+// resume-details
+const adminSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    name: String,
+    surname: String,
+    nameFa: String,
+    surnameFa: String,
+    isAdmin: Boolean,
+}, {
+    timestamps: true,
+});
+
+/**
+ * We can Add Bcrypt for hashing password 
+ */
+
+module.exports = mongoose.model('Admin', adminSchema);
